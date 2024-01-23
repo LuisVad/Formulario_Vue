d4bc3a7d0e63604db9fc526c4d5958ca5b0da057
LuisVad <20213tn002@utez.edu.mx>
2024-01-22 19:52:54 -0600

    22-01-2024

---
+++ formulario/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Formulario from './components/Formulario.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+  { path: '/', component: Formulario },
+  { path: '/exito', component: Exito }, // Define un componente para la página de éxito
+];
+
+const router = new VueRouter({
+  routes,
+});
+
+export default {
+  name: 'App',
+  router,
+};
+</script>
